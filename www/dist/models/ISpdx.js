57f4dd1310d36b50cded1c46645bb2d2524b3454
Martyn Janes <martyn@obany.com>
2017-09-28 11:47:33 +0300

    Added What's New

--- www/dist/models/ISpdx.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtb2RlbHMvSVNwZHguanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIifQ==
