b2599edbb460e893a788be718eee6fa66239e51c
Martyn Janes <martyn@obany.com>
2017-10-18 15:10:55 +0300

    Added docker info

--- www/src/navBar.ts
+++ www/src/navBar.ts
@@ -12,7 +12,7 @@ export class NavBar {
     public version: string;
 
     public attached(): void {
-        this.version = "Engine v1.2.7";
+        this.version = "Engine v1.4.0";
         // registry.npmjs.org CORS fails
         // const httpClient = new HttpClient();
         // httpClient.get("https://registry.npmjs.org/unitejs-cli/")
