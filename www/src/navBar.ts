6ed2d30c3186bd1a338d234269a3e797e5eb6b91
Martyn Janes <martyn@obany.com>
2017-11-11 09:43:47 +0300

    Update to api lookup

--- www/src/navBar.ts
+++ www/src/navBar.ts
@@ -1,6 +1,7 @@
 /**
  * NavBar class.
  */
+import { HttpClient } from "aurelia-fetch-client";
 import { Router } from "aurelia-router";
 import { bindable, customElement } from "aurelia-templating";
 
@@ -12,6 +13,17 @@ export class NavBar {
     public version: string;
 
     public attached(): void {
-        this.version = "Engine v1.6.6";
+        const httpClient = new HttpClient();
+        httpClient.fetch("https://api.npms.io/v2/package/unitejs-cli")
+        .then((response) => response.json())
+        .then((json) => {
+            if (json.collected &&
+                json.collected.metadata) {
+                this.version = `v${json.collected.metadata.version}`;
+            }
+        })
+        .catch(() => {
+            // ignoer error we just wont display version
+        });
     }
 }
